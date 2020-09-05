00a36a3ab43daf55856f04bbed4d64e270a2134e
Dominik Jancik <magnet.cz@gmail.com>
2020-09-05 17:07:58 +0200

    Added loading spinner

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,86 +1,91 @@
-import React, { PureComponent, Fragment } from "react";
-import Project from "./Project/Project";
-import * as contentful from "contentful";
-import Emoji from "../UI/Emoji/Emoji";
-import Masonry from "react-masonry-css";
-import { Flipper, Flipped } from "react-flip-toolkit";
+import React, { PureComponent, Fragment } from 'react'
+import Project from './Project/Project'
+import * as contentful from 'contentful'
+import Emoji from '../UI/Emoji/Emoji'
+import Masonry from 'react-masonry-css'
+import { Flipper, Flipped } from 'react-flip-toolkit'
+import Spinner from '../UI/Spinner/Spinner'
 
 const breakpointColumnsObj = {
   default: 3,
   1024: 2,
   640: 1,
-};
+}
 
 class Projects extends PureComponent {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       projects: [],
       sortDirection: true,
-    };
+      loading: true,
+    }
   }
 
   client = contentful.createClient({
-    space: "kexut3hz4rzv",
-    accessToken: "fT6_oaDOfRx8g5aScDq6SZBLEWGDQAaNWWUn6MvIdxw",
-  });
+    space: 'kexut3hz4rzv',
+    accessToken: 'fT6_oaDOfRx8g5aScDq6SZBLEWGDQAaNWWUn6MvIdxw',
+  })
 
   componentDidMount() {
-    this.fetchProjects().then(this.setProjects);
+    this.fetchProjects().then(this.setProjects)
   }
 
-  fetchProjects = () => this.client.getEntries({ content_type: "project" });
+  fetchProjects = () => this.client.getEntries({ content_type: 'project' })
 
   setProjects = (response) => {
     this.setState({
       projects: response.items,
-    });
-  };
+      loading: false,
+    })
+  }
 
   sortProjects = (items) => {
-    let sortedProjects = [...items];
+    let sortedProjects = [...items]
     sortedProjects.sort(
-      (a, b) => new Date(a.fields.startDate) - new Date(b.fields.startDate)
-    );
+      (a, b) => new Date(a.fields.startDate) - new Date(b.fields.startDate),
+    )
 
     if (this.state.sortDirection) {
-      sortedProjects.reverse();
+      sortedProjects.reverse()
     }
 
     const featuredProjects = sortedProjects.filter(
-      (project) => project.fields.featured
-    );
+      (project) => project.fields.featured,
+    )
     const nonFeaturedProjects = sortedProjects.filter(
-      (project) => !project.fields.featured
-    );
+      (project) => !project.fields.featured,
+    )
     // sortedProjects.sort(
     //   (a, b) => a.fields.featured - b.fields.featured
     // );
 
-    return featuredProjects.concat(nonFeaturedProjects);
-  };
+    return featuredProjects.concat(nonFeaturedProjects)
+  }
 
   handleClicked = () => {
-    this.setState((state) => ({ sortDirection: !state.sortDirection }));
-  };
+    this.setState((state) => ({ sortDirection: !state.sortDirection }))
+  }
 
   render() {
-    const projects = this.sortProjects(this.state.projects);
+    if (this.state.loading) return <Spinner />
+
+    const projects = this.sortProjects(this.state.projects)
     const projectElements = projects.map((project) => {
       return (
         <Flipped key={project.sys.id} flipId={project.sys.id}>
           {(flippedProps) => {
-            return <Project {...project.fields} flippedProps={flippedProps} />;
+            return <Project {...project.fields} flippedProps={flippedProps} />
           }}
         </Flipped>
-      );
-    });
+      )
+    })
 
-    const flipperKey = projects.map((project) => project.sys.id).join("");
+    const flipperKey = projects.map((project) => project.sys.id).join('')
 
     const directionBaseClasses =
-      "transform transition duration-500 inline-block ";
+      'transform transition duration-500 inline-block '
 
     return (
       <Fragment>
@@ -94,7 +99,7 @@ class Projects extends PureComponent {
               label="Direction"
               className={
                 directionBaseClasses +
-                (this.state.sortDirection ? "rotate-0" : "rotate-180")
+                (this.state.sortDirection ? 'rotate-0' : 'rotate-180')
               }
             >
               ⏩
@@ -109,12 +114,12 @@ class Projects extends PureComponent {
             className="my-masonry-grid"
             columnClassName="my-masonry-grid_column"
           >
-          {projectElements}
+            {projectElements}
           </Masonry>
         </Flipper>
       </Fragment>
-    );
+    )
   }
 }
 
-export default Projects;
+export default Projects
