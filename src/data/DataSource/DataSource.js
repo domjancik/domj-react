4ad4fcf3a7bc2d2d79fb93036a8f0c22e2fab75e
Dominik Jancik <magnet.cz@gmail.com>
2020-09-06 12:10:41 +0200

    Added extra DataSource signatures, without implementation

--- src/data/DataSource/DataSource.js
+++ src/data/DataSource/DataSource.js
@@ -3,11 +3,17 @@ import * as ContentfulClient from '../../util/contentful-client'
 import * as ContentTypes from './DataTypes'
 
 export const fetchProjects = () => ContentfulClient.getEntriesOfType(ContentTypes.PROJECT)
+export const fetchProject = ( id ) => {throw 'Not Implemented'}
 export const fetchCollections = () => ContentfulClient.getEntriesOfType(ContentTypes.PROJECT_COLLECTION)
+export const fetchCollection = ( id ) => {throw 'Not Implemented'}
+export const fetchCollectionBySlug = ( slug ) => {throw 'Not Implemented'}
 
 const DataSource = {
     fetchProjects,
-    fetchCollections
+    fetchProject,
+    fetchCollections,
+    fetchCollection,
+    fetchCollectionBySlug
 }
 
 export default DataSource
