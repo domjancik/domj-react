2ddacbe37cea8325df4b400568596a428729ca8d
Dominik Jancik <magnet.cz@gmail.com>
2020-09-06 23:38:34 +0200

    Updated footer text

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -5,7 +5,7 @@ import Icon from "../../UI/Icon/Icon";
 function Footer() {
   return (
     <div className="fixed text-right p-2 text-xs right-0 bottom-0 bg-white w-full z-50 hidden md:block">
-      (c) 2020; created using React, TailwindCSS, Contentful and VS Code;{" "}
+      (c) 2020 Dominik Jancik; created using React, TailwindCSS, Contentful and VS Code;{" "}
       <ExternalLink href="https://github.com/domjancik/domj-react">
         <Icon icon="github" />
       </ExternalLink>
