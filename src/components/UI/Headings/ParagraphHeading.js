1be44cfd8a8962f631a3fbe41fc93f8390aa95bf
Dominik Jancik <magnet.cz@gmail.com>
2020-09-06 18:46:25 +0200

    Removed border from paragraph heading

--- src/components/UI/Headings/ParagraphHeading.js
+++ src/components/UI/Headings/ParagraphHeading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ParagraphHeading(props) {
     return (
-        <h2 className={`text-sm text-teal-300 border-t-4 border-teal-200 border-dotted pt-2 ${props.className}`}>{props.children}</h2>
+        <h2 className={`text-center text-sm text-teal-300 pt-2 ${props.className}`}>{props.children}</h2>
     )
 }
 
