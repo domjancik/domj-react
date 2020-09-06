5a78aa817c8360bcebc1a5be152ff54ec6df8713
Dominik Jancik <magnet.cz@gmail.com>
2020-09-06 21:46:11 +0200

    Flickering Pills

--- src/components/UI/Pill/Pill.js
+++ src/components/UI/Pill/Pill.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Flicker from "../../../hoc/Animation/Flicker/Flicker"
+
 function Pill(props) {
   return (
-    <div className="inline-block rounded-full bg-teal-300 text-white px-4 mx-2 my-1">
+    <Flicker className="inline-block rounded-full text-white px-4 mx-2 my-1 transition-colors duration-500" flickerClass={['bg-teal-600', 'bg-teal-300']}>
       {props.children}
-    </div>
+    </Flicker>
   );
 }
 
