7cc064391dface78fad80428163f63e16bc384bf
Dominik Jancik <magnet.cz@gmail.com>
2020-09-07 20:42:42 +0200

    Removed log

--- src/App.js
+++ src/App.js
@@ -2,37 +2,96 @@ import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import Projects from "./components/Projects/Projects";
 import Hero from "./components/Hero/Hero";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-import Login from "./components/Login/Login";
+import { Route, Switch, Redirect, useLocation } from "react-router-dom";
 import AuthStore from "./store/AuthStore/AuthStore";
-import About from "./components/About/About";
 import Navigation from "./components/Layout/Navigation/Navigation";
 import CollectionDetail from "./components/Collections/CollectionDetail/CollectionDetail";
 
-function App() {
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: "-4rem",
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+    y: "4rem",
+  },
+};
+
+const pageTransition = {
+  type: "tween",
+  ease: "easeOut",
+  duration: 0.5
+};
+
+const pageTransitionOut = {
+  type: "tween",
+  ease: "easeIn",
+  duration: 10
+}
+
+function MotionDiv(props) {
+  const [animationComplete, setAnimationComplete] = useState(false)
+
+  const onAnimationComplete = () => {
+    setAnimationComplete(true)
+  }
+
   return (
-    <Router>
-      <AuthStore>
-        <Layout>
-          <Hero />
-          <Navigation />
-          <div className="min-h-screen">
-            <Switch>
-              <Route path="/login" component={Login} />
-              <Route path="/about" component={About} />
-              <Route path="/projects/:id" component={CollectionDetail} />
-              <Route path="/projects" component={Projects} />
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      onAnimationComplete={onAnimationComplete}
+      transition={animationComplete ? pageTransitionOut : pageTransition}
+      {...props}
+    >
+      {props.children}
+    </motion.div>
+  );
+}
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <AuthStore>
+      <Layout>
+        <Hero />
+        <Navigation />
+        <div className="min-h-screen">
+          <AnimatePresence exitBeforeEnter>
+            <Switch location={location} key={location.pathname}>
+              <Route
+                path="/projects/:id"
+                children={
+                  <MotionDiv>
+                    <CollectionDetail />
+                  </MotionDiv>
+                }
+              />
+              <Route
+                path="/projects"
+                children={
+                  <MotionDiv>
+                    <Projects />
+                  </MotionDiv>
+                }
+              />
               <Route path="/" children={<Redirect to="/projects" />} />
             </Switch>
-          </div>
-        </Layout>
-      </AuthStore>
-    </Router>
+          </AnimatePresence>
+        </div>
+      </Layout>
+    </AuthStore>
   );
 }
 export default App;
