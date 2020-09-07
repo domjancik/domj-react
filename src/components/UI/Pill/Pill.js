f856f07c0ebd723dd56c1ffe6eb226100c59297b
Dominik Jancik <magnet.cz@gmail.com>
2020-09-07 23:54:10 +0200

    Adjusted flickering

--- src/components/UI/Pill/Pill.js
+++ src/components/UI/Pill/Pill.js
@@ -4,7 +4,7 @@ import Flicker from "../../../hoc/Animation/Flicker/Flicker"
 
 function Pill(props) {
   return (
-    <Flicker style={{boxShadow: '0 3px black'}} className="inline-block rounded-full text-white px-4 mx-2 my-1 transition-colors duration-500" flickerClass={['bg-teal-600', 'bg-teal-300']}>
+    <Flicker probability={0.95} style={{boxShadow: '0 3px black'}} className="inline-block rounded-full text-white px-4 mx-2 my-1 transition-colors duration-500" flickerClass={['bg-teal-600', 'bg-teal-300']}>
       {props.children}
     </Flicker>
   );
